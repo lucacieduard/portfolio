40f635601608cc4d1250b28d452939b01d9434e9
Eduard Lucaci <eduardlucaci2002@yahoo.com>
2023-06-16 14:39:21 +0300

    ticket component added

---
+++ src/app/components/ticket/Ticket.jsx
@@ -0,0 +1,25 @@
+"use client";
+import React from "react";
+import styles from "./ticket.module.css";
+import { useContext } from "react";
+import { ThemeContext } from "@/app/context/ThemeContext";
+
+export const Ticket = (props) => {
+  const context = useContext(ThemeContext);
+  console.log(context);
+
+  let color;
+
+  if (context.mode === "dark") {
+    color = "#172135";
+  } else {
+    color = "#E3E8ED";
+  }
+
+  return (
+    <div className={styles.container} style={{ background: color }}>
+      {props.icon && <span className={styles.icon}>{props.icon}</span>}{" "}
+      <span className={styles.name}>{props.name}</span>
+    </div>
+  );
+};
