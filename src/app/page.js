6199a6507c8c90616c8aa9ae069f7222a0e4cb3b
Eduard Lucaci <eduardlucaci2002@yahoo.com>
2023-06-18 13:32:47 +0300

    -

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import { ExperienceSection } from "./components/exprerienceSection/Experience";
 import { Hero } from "./components/heroSection/Hero";
+import WorkSection from "./components/workSection/WorkSection";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <ExperienceSection />
+      <WorkSection />
     </>
   );
 }
