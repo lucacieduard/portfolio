0793d40c1536ee69d19556b2e482cc7efb7f43ca
Eduard Lucaci <eduardlucaci2002@yahoo.com>
2023-06-23 18:35:27 +0300

    responsive workSection

--- src/app/components/workSection/WorkSection.jsx
+++ src/app/components/workSection/WorkSection.jsx
@@ -1,71 +1,51 @@
-import React from "react";
+"use client";
+
+import React, { useContext } from "react";
 import styles from "./workSection.module.css";
-import Image from "next/image";
 import { ImgCarouse } from "../imgCarousel/ImgCarouse";
 import { Ticket } from "../ticket/Ticket";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCss3, faHtml5, faReact } from "@fortawesome/free-brands-svg-icons";
+import workContent from "@/app/utils/workContent";
+import { ThemeContext } from "@/app/context/ThemeContext";
 
 const WorkSection = () => {
+  const { toggle, mode } = useContext(ThemeContext);
+  let style;
+
+  if (mode === "dark") {
+    style = { backgroundColor: "#172135" };
+  } else {
+    style = { backgroundColor: "#E3E8ED" };
+  }
   return (
     <section className={styles.container} id="workSection">
       <h2 className={styles.title}>
         My latest <span style={{ color: "#2563EB" }}>work</span>
       </h2>
       <div className={styles.projects}>
-        <div className={styles.project}>
-          <h3 className={styles.projectTitle}>meTEO, Weather APP</h3>
-          <ImgCarouse />
-          <div className={styles.socialContainer}>
-            <ul className={styles.tehnology}>
-              <li>
-                {" "}
-                <Ticket icon={<FontAwesomeIcon icon={faHtml5} />} name="HTML" />
-              </li>
-              <li>
-                {" "}
-                <Ticket icon={<FontAwesomeIcon icon={faCss3} />} name="CSS" />
-              </li>
-              <li>
-                {" "}
-                <Ticket
-                  icon={<FontAwesomeIcon icon={faReact} />}
-                  name="React"
-                />
-              </li>
-            </ul>
-          </div>
-          <p className={styles.description}>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque
-          </p>
-        </div>
-
-        <div className={styles.project}>
-          <h3 className={styles.projectTitle}>meTEO, Weather APP</h3>
-          <ImgCarouse />
-          <div className={styles.socialContainer}>
-            <ul className={styles.tehnology}>
-              <li>
-                {" "}
-                <Ticket icon={<FontAwesomeIcon icon={faHtml5} />} name="HTML" />
-              </li>
-              <li>
-                {" "}
-                <Ticket icon={<FontAwesomeIcon icon={faCss3} />} name="CSS" />
-              </li>
-              <li>
-                {" "}
-                <Ticket
-                  icon={<FontAwesomeIcon icon={faReact} />}
-                  name="React"
-                />
-              </li>
-            </ul>
-          </div>
-          <p className={styles.description}>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque
-          </p>
-        </div>
+        {workContent.map((project, index) => {
+          return (
+            <div
+              className={styles.project}
+              key={project.id}
+              style={index === 0 ? style : {}}
+            >
+              <h3 className={styles.projectTitle}>{project.title}</h3>
+              <ImgCarouse images={project.images} />
+              <div className={styles.socialContainer}>
+                <ul className={styles.tehnology}>
+                  {project.tehnologies.map((thenology) => {
+                    return (
+                      <li key={thenology.name}>
+                        <Ticket icon={thenology.icon} name={thenology.name} />
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+              <p className={styles.description}>{project.description}</p>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
