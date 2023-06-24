9286ed541750c5cb6031302f3b101c166fa71b05
Eduard Lucaci <eduardlucaci2002@yahoo.com>
2023-06-24 10:44:54 +0300

    new contact links added

--- src/app/components/contactSection/Contact.jsx
+++ src/app/components/contactSection/Contact.jsx
@@ -3,6 +3,14 @@
 import { useContext } from "react";
 import styles from "./contact.module.css";
 import { ThemeContext } from "@/app/context/ThemeContext";
+import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faGithub,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faMailReply } from "@fortawesome/free-solid-svg-icons";
 export const Contact = () => {
   const { mode, toggle } = useContext(ThemeContext);
   return (
@@ -17,6 +25,20 @@ export const Contact = () => {
     >
       <p>Ready to start your next project?</p>
       <p>Get in touch me!</p>
+      <div className={styles.socialContainer}>
+        <Link href="https://linkedin.com/in/eduard-lucaci" target="_blank">
+          <FontAwesomeIcon icon={faLinkedin} />
+        </Link>
+        <Link href="https://facebook.com/eduard.lucaci.3/" target="_blank">
+          <FontAwesomeIcon icon={faFacebook} />
+        </Link>
+        <Link href="https://github.com/lucacieduard" target="_blank">
+          <FontAwesomeIcon icon={faGithub} />
+        </Link>
+        <Link href="mailto:eduardiosif.lucaci@gmail.com">
+          <FontAwesomeIcon icon={faEnvelope} />
+        </Link>
+      </div>
     </section>
   );
 };
