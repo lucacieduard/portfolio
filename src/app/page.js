e56abc94b539abdfa1320df5090d0861135768e6
Eduard Lucaci <eduardlucaci2002@yahoo.com>
2023-11-09 12:07:00 +0200

    analytics added

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,12 @@ import { Contact } from "./components/contactSection/Contact";
 import { ExperienceSection } from "./components/exprerienceSection/Experience";
 import { Hero } from "./components/heroSection/Hero";
 import WorkSection from "./components/workSection/WorkSection";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   return (
     <>
+      <Analytics />
       <Hero />
       <ExperienceSection />
       <WorkSection />
